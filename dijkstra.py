5ae607221be2db3ab644b65edac8449c623c1879
zxteloiv <zxteloiv@gmail.com>
2016-02-25 09:34:57 +0800

    add multidest dijkstra and refactor the code and fix a misterious bug that causes endless loop from src 0 to dest 4

--- dijkstra.py
+++ dijkstra.py
@@ -3,14 +3,7 @@
 
 from priority_queue import MinPriorityQueue
 
-def dijkstra_single_path(src, dest, vertices, edges):
-    """
-    Get the shortest path from src to dest.
-    :param src: index for the source vertex
-    :param dest: index for the destination vertex
-    :param vertices: a list of vertices
-    :param edges: a list of edges (src, dest, weight)
-    """
+def _dijkstra_single_src_compute(src, vertices, edges):
 
     # init 
     dist_to = [4294967295] * len(vertices)
@@ -33,14 +26,20 @@ def dijkstra_single_path(src, dest, vertices, edges):
         visited.add(vertex)
         # find all edges starting from the vertex, and relax the edge.
         # in an undirect graph, both src and dest of an edge is considered 
-        edges_on_vertex = (edge for edge in edges if vertex in edge)
+        edges_on_vertex = (edge for edge in edges
+                if (vertex == edge[0] or vertex == edge[1])
+                and (edge[0] not in visited or edge[1] not in visited))
 
         for edge in edges_on_vertex: # ????
             other_vertex = edge[0] if edge[0] not in visited else edge[1]
+
             if dist_to[other_vertex] > dist_to[vertex] + edge[2]:
                 dist_to[other_vertex] = dist_to[vertex] + edge[2]
                 edge_to[other_vertex] = edge
 
+    return dist_to, edge_to
+
+def _find_route(src, dest, edge_to):
     # find the shortest route
     route = [dest]
     vertex = dest
@@ -50,11 +49,59 @@ def dijkstra_single_path(src, dest, vertices, edges):
         vertex = arrow_tail
 
     route = list(reversed(route))
+    return route
+
+def dijkstra_multiple_dest(src, dest_list, vertices, edges):
+    """
+    Get the shortest path from src to several dest.
+    :param src: index for the source vertex
+    :param dest_list: a list of index for the destination vertex
+    :param vertices: a list of vertices
+    :param edges: a list of edges (src, dest, weight)
+
+    :return list of (shortest_dist, route) tuple where each item is for a dest
+    """
+    dist_to, edge_to = _dijkstra_single_src_compute(src, vertices, edges)
+    return list((dist_to[dest], _find_route(src, dest, edge_to))
+            for dest in dest_list)
+
+def dijkstra_single_path(src, dest, vertices, edges):
+    """
+    Get the shortest path from src to dest.
+    :param src: index for the source vertex
+    :param dest: index for the destination vertex
+    :param vertices: a list of vertices
+    :param edges: a list of edges (src, dest, weight)
+
+    :return tuple of (the_shortest_distance, route)
+            where the shortest distance is a number,
+            and the route is a list of vertices from the src to dest
+    """
+    dist_to, edge_to = _dijkstra_single_src_compute(src, vertices, edges)
+    route = _find_route(src, dest, edge_to)
 
     return (dist_to[dest], route)
 
 if __name__ == "__main__":
 
+    # extreme case
+    vertices = range(1)
+    edges = []
+    (dist, route) = dijkstra_single_path(0, 0, vertices, edges)
+    print "\n===> should print out: dist=0 route=(0)"
+    print "dist=>", dist, "\troute=>", route
+
+    # error case
+    vertices = []
+    edges = []
+    print "\n===> should raise exception XXX"
+    import traceback
+    try:
+        (dist, route) = dijkstra_single_path(0, 0, vertices, edges)
+    except Exception as e:
+        print traceback.format_exc()
+
+    # from 0 to 5
     vertices = range(6)
     edges = [(0, 1, 1), (0, 2, 1), # v0
             (1, 3, 1), (2, 3, 1), (2, 4, 1),
@@ -63,27 +110,14 @@ if __name__ == "__main__":
 
     (dist, route) = dijkstra_single_path(0, 5, vertices, edges)
 
-    print "should print out: dist=3 route=(0, 1, 3, 5) or anything similar"
+    print "\n===> should print out: dist=3 route=(0, 1, 3, 5) or anything similar"
     print "dist=>", dist, "\troute=>", route
 
-    # extreme case
+    # multiple path from 0 to all other vertices
+    print ("\n===> should print out: dist=1 1 2 2 3 and " +
+        "[0,1], [0,2], [0,1,3], [0,2,4], [0,1,3,5]")
+    for (dist, route) in dijkstra_multiple_dest(0, [1,2,3,4,5], vertices, edges):
+        print "dist=>", dist, "\troute=>", route
 
-    vertices = range(1)
-    edges = []
-    (dist, route) = dijkstra_single_path(0, 0, vertices, edges)
-    print "should print out: dist=0 route=(0)"
-    print "dist=>", dist, "\troute=>", route
-
-    # error case
-    vertices = []
-    edges = []
-    print "should raise exception XXX"
-    import traceback
-    try:
-        (dist, route) = dijkstra_single_path(0, 0, vertices, edges)
-    except Exception as e:
-        print traceback.format_exc()
-
-    # blabla
 
 
