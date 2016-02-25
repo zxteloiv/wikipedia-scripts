9cdf540d78456d80d3283a081a2d20cd1f2afb69
Xiang Zhang <zxteloiv@gmail.com>
2016-02-25 19:52:33 +0800

    add get_token_id_list_by_mention function and corresponding unittest

--- depparse.py
+++ depparse.py
@@ -44,5 +44,52 @@ def get_sentence_id(sentences, start, end):
 
     return i
 
+def get_token_id_list_by_mention(mention, sentences):
+    """ Given a mention, find all the tokens with its span """
+    (mstart, mend, href, _) = mention
+    sentence_id = get_sentence_id(sentences, mstart, mend)
+
+    if sentence_id and u'tokens' not in sentences:
+        raise ValueError('sentence structure invalid')
+    tokens = sentences[sentence_id][u'tokens']
+
+    token_id_list = []
+    for (i, token) in enumerate(tokens):
+        tstart, tend = token['characterOffsetBegin'], token['characterOffsetEnd']
+        # tokens is inside a mention span
+        if mstart <= tstart and tend <= mend:
+            token_id_list.append(i)
+
+        # token is iterated beyond the current mention, quickly break
+        # ----(mention)---(tokens)----
+        if mend < tstart:
+            break
+
+    return token_id_list
+
 if __name__ == "__main__":
+    from utils import charset_wrapper
+    from wiki_doc import wikiobj_to_doc
+    from entity_mentions import get_entity_mentions, get_entity_mentions_in_lines
+    from entity_mentions import get_plain_text, get_plain_text_mention_info
+    import sys
+
+    doc = wikiobj_to_doc(charset_wrapper(open(sys.argv[1], 'r'))).next()
+    nlp = init_corenlp('http://localhost', 9000)
+
+    for line in doc['text']:
+        line = line.rstrip()
+        if not line: continue
+
+        plaintext = get_plain_text(line)
+        mentions = get_plain_text_mention_info(line)
+        if not mentions: continue
+
+        depparsed_output = depparse_paragraph(plaintext, nlp)
+        sentences = depparse_paragraph['sentences']
+
+        token_id_list = get_token_id_list_by_mention(mentions[0], sentences)
+        print token_id_list
+
+
     pass
