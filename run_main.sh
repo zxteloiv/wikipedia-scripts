4c9518ea846bd6a0ddb3197c301673fcf4f3beee
zxteloiv <zxteloiv@gmail.com>
2016-02-28 10:54:59 +0800

    change dev env to dell pc and modify the path

--- run_main.sh
+++ run_main.sh
@@ -8,7 +8,7 @@ python $4 main.py \
     --sys-log-path log \
     --state-path log/state \
     --wiki-file $1 \
-    --entity-wiki-file /data/home/zxteloiv/kbc/mid2wiki.txt \
+    --entity-wiki-file /home/zxteloiv/data/kbc/mid2wiki.txt \
     --entity-sentence-output-file $2 \
     --task-id $3 \
     --log-level DEBUG \
