2075c8e83b4194fd99309b499baabeda5d779e0c
zxteloiv <zxteloiv@gmail.com>
2016-03-13 19:09:17 +0800

    add new test sample, fix bug that add one token id to list for multiple times

--- run_test_sentence_entity.sh
+++ run_test_sentence_entity.sh
@@ -6,10 +6,10 @@ mkdir -p log/state
 python $3 main_sentence_entity.py \
     --sys-log-path log \
     --state-path log/state \
-    --entity-wiki-file /data/home/zxteloiv/data/wiki_resources/mid2wiki.txt \
-    --wiki-redirect-file /data/home/zxteloiv/data/wiki_resources/redirect_title \
+    --entity-wiki-file /home/zxteloiv/data/wiki_resources/mid2wiki.txt \
+    --wiki-redirect-file /home/zxteloiv/data/wiki_resources/redirect_title \
     --wiki-file $1 \
     --task-id $2 \
-    --log-level INFO \
+    --log-level DEBUG \
     --entity-output-file log/entity-output \
 
