e14bdb8d2c2de1dd2d09ce19850da9a20d2cd387
zxteloiv <zxteloiv@gmail.com>
2016-03-28 19:40:16 +0800

    untested code ready for import data to redis server

--- event_sentence_alignment.py
+++ event_sentence_alignment.py
@@ -27,30 +27,54 @@ parser.add_argument('--output-file')
 
 from utils import charset_wrapper
 
-def build_event_index(robj, event_schema, event_file):
-    keyproperties, evtype, event = {}, None, []
+def build_key_properties_table(event_schema_file):
+    """
+    Read the schema file, return a object as {event_type: set_of_key_properties}
+    """
+    keytable, evtype, event = {}, None, set()
     for line in charset_wrapper(open(event_schema_file)):
         line = line.rstrip()
 
         if line.startswith(u'\t'):
-            event.append(line.lstrip())
+            event.add(line.lstrip())
         else:
             if evtype is not None:
-                keyproperties[evtype] = event
+                keytable[evtype] = event
             evtype = line
-            event = []
+            event = set()
 
-    keyproperties[evtype] = event
+    keytable[evtype] = event
+    return keytable
 
+def build_event_index(robj, event_schema, event_file):
+    """
+    Read every event instance in the event_file, if it is valid by referring to 
+    the event schema, write it into redis as:
+        key = "ev_" + key_prop_1 + key_prop_2
+        val = event { id:ev_id, type:evtype, prop1:val1, prop2:val2, ... }
+    """
     events = json.loads(open(event_file).read())
-
     for (ev_id, ev_data) in events.iteritems():
         if len(ev_data) <= 1:
             continue
         evtype = ev_data[0]
+        try:
+            # get the list of key properties contained in the event instance
+            keyprops = [k for (k, _) in (item.split(u'\t') for item in ev_data[1:])
+                    if evtype in event_schema and k in event_schema[evtype]]
+        except Exception:
+            continue
 
-        if any() in (kvpair.split('\t') for kvpair in ev_data[1:]):
-            pass
+        if len(keyprops) < 2: # key properties must be at least 2
+            continue
+
+        event = {'id': ev_id, 'type': evtype}
+        event.update(kvpair.split('\t')[1] for kvpair in ev_data[1:])
+
+        rkey = 'ev_' + ''.join(x.decode('utf-8') for x in event_schema[evtype])
+        rval = json.encode(event)
+
+        robj.set(rkey, rval)
 
 def mention_pairs(mentions):
     """
