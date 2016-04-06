f57bd667c39d645c5a5acb00ed8b90ee7eff36b5
zxteloiv <zxteloiv@gmail.com>
2016-04-06 13:49:24 +0800

    make the redis key composed of mid rather than raw entity string

--- event_sentence_alignment.py
+++ event_sentence_alignment.py
@@ -18,10 +18,12 @@ parser.add_argument('--nlp-port', default=9000)
 
 parser.add_argument('--redis-server', default='127.0.0.1')
 parser.add_argument('--redis-port', default=6379)
-parser.add_argument('--redis-db', default=0)
+parser.add_argument('--redis-db', default=1)
 
 parser.add_argument('--key-schema-file', help='file of keys in event schema')
 parser.add_argument('--event-file', help='event instance file')
+parser.add_argument('--mid-entity-file', help='the mid \\t entity file to load')
+
 parser.add_argument('--sentence-entity', help='wiki sentence and entity')
 parser.add_argument('--output-file')
 
@@ -46,33 +48,53 @@ def build_key_properties_table(event_schema_file):
     keytable[evtype] = event
     return keytable
 
-def build_event_index(robj, event_schema, event_file):
+def build_string_mid_table(mid_to_entity_file):
+    """ Turn a tab-separated file <mid, string> into a dict <string, mid> """
+    return dict((s.strip(), m) for (m, s) in (line.rstrip().split('\t')
+        for line in charset_wrapper(open(mid_to_entity_file, 'r'))
+        ))
+
+def make_rkey(evtype, keyprops):
+    rkey = evtype + u''.join(sorted(keyprops))
+    return rkey.encode('utf-8')
+
+def build_event_index(robj, event_schema, string_to_mid, event_file):
     """
     Read every event instance in the event_file, if it is valid by referring to 
     the event schema, write it into redis as:
         key = evtype + key_prop_1 + key_prop_2
         val = event { id:ev_id, type:evtype, prop1:val1, prop2:val2, ... }
+
     """
     events = json.loads(open(event_file).read())
+    syslog = xuxian.log.system_logger
     for (ev_id, ev_data) in events.iteritems():
         if len(ev_data) <= 1:
             continue
         evtype = ev_data[0]
         try:
             # get the list of key properties contained in the event instance
-            keyprops = dict((k, v)
+            keyprops = dict((k, string_to_mid[v] if v in string_to_mid else v)
                     for (k, v) in (item.split(u'\t') for item in ev_data[1:])
                     if evtype in event_schema and k in event_schema[evtype])
         except Exception:
+            syslog.debug(ev_id.encode('utf-8') + "\tkeypropsfailure")
             continue
 
         if len(keyprops) < 2: # key properties must be at least 2
+            syslog.debug(ev_id.encode('utf-8') + "\tlessthan2")
             continue
 
         event = {u'id': ev_id, u'type': evtype}
-        event.update(kvpair.split(u'\t') for kvpair in ev_data[1:])
+        event.update((k, string_to_mid[v] if v in string_to_mid else v)
+                for (k, v) in (kvpair.split(u'\t') for kvpair in ev_data[1:]))
 
-        rkey = evtype.encode('utf-8') + ''.join(x[1].encode('utf-8') for x in keyprops.items())
+        if any(v not in string_to_mid
+                for (k, v) in (item.split(u'\t') for item in ev_data[1:])
+                if evtype in event_schema and k in event_schema[evtype]):
+            syslog.info(ev_id.encode('utf-8') + "\tkeynotfound\t" + str(keyprops))
+
+        rkey = make_rkey(evtype, keyprops.values())
         rval = json.dumps(event)
 
         rtn = robj.set(rkey, rval)
@@ -108,7 +130,8 @@ def main(args):
     #entity_outfile = xuxian.apply_dump_file('entity', args.single_entity_output_file)
 
     event_schema = build_key_properties_table(args.key_schema_file)
-    build_event_index(robj, event_schema, args.event_file)
+    string_to_mid = build_string_mid_table(args.mid_entity_file)
+    build_event_index(robj, event_schema, string_to_mid, args.event_file)
 
     # iterate over data input
     pass
