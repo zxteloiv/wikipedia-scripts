551f3d57c46248effc6f01958b3ad2507beb166d
zxteloiv <zxteloiv@gmail.com>
2017-08-21 05:32:57 +0800

    fix the memory leaks caused by opencc

--- cirrus/filter_category.py
+++ cirrus/filter_category.py
@@ -7,6 +7,47 @@ import re, sys, os.path
 import logging
 import opencc
 
+cc = opencc.OpenCC()
+
+class CategoryResourcesPyNative(object):
+    def __init__(self, rfile, cfile):
+        self.rfile = rfile
+        self.cfile = cfile
+
+        self._redir = {}
+        self._kinship = {}
+
+        def strip_and_codec(obj, enc='utf-8'):
+            for l in obj:
+                yield l.rstrip().decode(enc)
+
+        # build redirect map
+        self._insert_arity_2_stream(strip_and_codec(self.rfile), self._redir)
+        logging.info("inserted records amount: %d" % len(self._redir))
+
+        # build kinship map
+        self._insert_arity_2_stream(strip_and_codec(self.cfile), self._kinship)
+        logging.info("inserted records amount: %d" % len(self._kinship))
+
+    def _insert_arity_2_stream(self, stream, target, sep='\t'):
+        for line in stream:
+            parts = line.split(sep)
+            assert (len(parts) == 2)
+            if parts[0] not in target:
+                target[parts[0]] = []
+
+            target[parts[0]].append(parts[1])
+
+    def query_category_redirect(self, category):
+        if category in self._redir:
+            return self._redir[category][0]
+        return None
+
+    def query_category_uplevel(self, category):
+        if category in self._kinship:
+            return self._kinship[category]
+        return []
+
 class CategoryResources(object):
     def __init__(self, rfile, cfile):
         self.rfile = rfile
@@ -51,26 +92,35 @@ class CategoryResources(object):
         return self.cur.fetchone()[0]
 
     def _insert_arity_2_stream(self, stream, ins_template, sep='\t'):
+        cur = self.conn.cursor()
         for line in stream:
             parts = line.split(sep)
             assert (len(parts) == 2)
 
             try:
-                self.cur.execute(ins_template, (parts[0], parts[1]))
+                cur.execute(ins_template, (parts[0], parts[1]))
             except:
                 continue
 
     def query_category_redirect(self, category):
-        self.cur.execute('select c_to from category_redirect where c_from=?', (category, ))
-        res = self.cur.fetchone()
+        cur = self.conn.cursor()
+        cur.execute('select c_to from category_redirect where c_from=?', (category, ))
+        res = cur.fetchone()
         if res is not None:
             res = res[0]
         return res
 
     def query_category_uplevel(self, category):
-        self.cur.execute('select c_from, c_to from category_kinship where c_from=?', (category, ))
-        res = [x[1] for x in self.cur.fetchall()]
+        cur = self.conn.cursor()
+        cur.execute('select c_from, c_to from category_kinship where c_from=?', (category, ))
+        res = [x[1] for x in cur.fetchall()]
         return res
+
+def redirect(c, cr):
+    target = cr.query_category_redirect(c)
+    if target is None:
+        target = c
+    return target
 
 def traceroute(cr, categories, domain, max_len=12):
     """
@@ -78,15 +128,9 @@ def traceroute(cr, categories, domain, max_len=12):
     Code is copied and modified from the filter_category_kinship_by_domain function.
     """
     visited = set()
-    domain = opencc.convert(domain)
+    domain = cc.convert(domain)
 
-    def redirect(c):
-        target = cr.query_category_redirect(c)
-        if target is None:
-            target = c
-        return target
-
-    buf = [[redirect(opencc.convert(c))] for c in categories]
+    buf = [[redirect(cc.convert(c), cr)] for c in categories]
     layer = 0
     while len(buf) > 0 and layer < max_len:
         for path in buf:
@@ -98,8 +142,8 @@ def traceroute(cr, categories, domain, max_len=12):
 
         for path in buf:
             category = path[-1]
-            category = opencc.convert(category)
-            category = redirect(category)
+            category = cc.convert(category)
+            category = redirect(category, cr)
             uplevel_categories = cr.query_category_uplevel(category)
             next_buf.extend(path + [c] for c in uplevel_categories if c not in visited)
 
@@ -110,27 +154,18 @@ def traceroute(cr, categories, domain, max_len=12):
 
 def filter_category_kinship_by_domain(cr, categories, domain, max_len=12):
     visited = set()
-    domain = opencc.convert(domain)
+    domain = cc.convert(domain)
 
-    def redirect(c):
-        target = cr.query_category_redirect(c)
-        if target is None:
-            target = c
-        return target
-
-    buf = set(redirect(opencc.convert(x)) for x in categories)
+    buf = set(redirect(cc.convert(x), cr) for x in categories)
     layer = 0
     while len(buf) > 0 and layer < max_len:
         if domain in buf: return True
         visited.update(buf)
         next_buf = set()
 
-        #logging.debug('visited:' + repr(visited).decode('unicode_escape').encode('utf-8'))
-        logging.debug('buf:' + u','.join(buf).encode('utf-8'))
-
         for category in buf:
-            category = opencc.convert(category)
-            category = redirect(category)
+            category = cc.convert(category)
+            category = redirect(category, cr)
             uplevel_categories = cr.query_category_uplevel(category)
             next_buf.update(x for x in uplevel_categories if x not in visited)
 
@@ -139,20 +174,24 @@ def filter_category_kinship_by_domain(cr, categories, domain, max_len=12):
 
     return False
 
-def test():
-    parser = argparse.ArgumentParser(description="read json and filter items by domain")
-
-    parser.add_argument("input", help="input file")
-    parser.add_argument("-r", "--category_redir_map", type=file, help="redirect map for category, a tsv file")
-    parser.add_argument("-c", "--category_parent_map", type=file, help="category parent lookup, a tsv file")
-    parser.add_argument("-d", "--domain", help="the required domain to filter docs, e.g. Botany")
-    parser.add_argument("-q", "--quiet", help="mute the log")
-    args = parser.parse_args()
-
-    if not args.quiet:
-        logging.getLogger().setLevel(logging.INFO)
-
+def filter_cirrus_category(args):
+    
     cr = CategoryResources(rfile=args.category_redir_map, cfile=args.category_parent_map)
+    domain = args.domain.decode('utf-8')
+    output = open(args.output, 'w')
+    for i, line in enumerate(open(args.input)):
+        page = json.loads(line.rstrip())
+        if i % 10000:
+            logging.info(i * 10000)
+            
+        if filter_category_kinship_by_domain(cr, page['categories'], domain, max_len=10):
+            output.write(line)
+
+def test(args):
+    import datetime
+    print 'before:', datetime.datetime.now()
+    cr = CategoryResourcesPyNative(rfile=args.category_redir_map, cfile=args.category_parent_map)
+    print 'after:', datetime.datetime.now()
 
     print 'redirect: Fatty_acids ->', cr.query_category_redirect(u'Fatty acids')
     print 'level up: 海岸山脉 ->', repr(cr.query_category_uplevel(u'海岸山脉')).decode('unicode_escape')
@@ -166,6 +205,27 @@ def test():
     print filter_category_kinship_by_domain(cr, [u'各国墓葬'], u'宗教場所'), '各国墓葬 belongs to 宗教場所' 
     print filter_category_kinship_by_domain(cr, [u'各国墓葬'], u'宗教场所'), '各国墓葬 belongs to 宗教场所' 
 
+def main():
+    parser = argparse.ArgumentParser(description="read json and filter items by domain")
+
+    parser.add_argument("-i", "--input", help="input file")
+    parser.add_argument("-f", "--filelist", help="a filename sheet contains all the needed input files")
+    parser.add_argument("-r", "--category_redir_map", type=file, help="redirect map for category, a tsv file")
+    parser.add_argument("-c", "--category_parent_map", type=file, help="category parent lookup, a tsv file")
+    parser.add_argument("-o", "--output", help="output json filename")
+    parser.add_argument("-d", "--domain", help="the required domain to filter docs, e.g. Botany")
+    parser.add_argument("-q", "--quiet", action="store_true", help="mute the log")
+    parser.add_argument("--test", action="store_true", help="run the test entry")
+    args = parser.parse_args()
+
+    if not args.quiet:
+        logging.getLogger().setLevel(logging.INFO)
+
+    if args.test:
+        test(args)
+    else:
+        filter_cirrus_category(args)
+
 if __name__ == "__main__":
-    test()
+    main()
 
