62cc181ad01d149b0b9a5413deb72a2bfbb87f3f
zxteloiv <zxteloiv@gmail.com>
2017-08-21 00:13:56 +0800

    use class to wrap database uses

--- cirrus/filter_category.py
+++ cirrus/filter_category.py
@@ -6,6 +6,71 @@ import argparse
 import re, sys, os.path
 import logging
 
+class CategoryResources(object):
+    def __init__(self, rfile, cfile):
+        self.rfile = rfile
+        self.cfile = cfile
+        self.conn = sqlite3.connect(':memory:')
+
+        self._create_tables = """
+            create table category_redirect (
+                c_from text,
+                c_to text,
+                primary key (c_from, c_to)
+            );
+
+            create table category_kinship (
+                id integer primary key autoincrement,
+                c_from text,
+                c_to text
+            );
+
+            create index c_from_index on category_kinship(c_from);
+            """
+
+        self.cur = self.conn.cursor()
+        self.cur.executescript(self._create_tables)
+
+        def strip_and_codec(obj, enc='utf-8'):
+            for l in obj:
+                yield l.rstrip().decode(enc)
+
+        # build redirect map
+        self._insert_arity_2_stream(strip_and_codec(self.rfile),
+                'insert into category_redirect values (?, ?)')
+        logging.info("inserted records amount: %d" % self._count_table('category_redirect'))
+
+        # build kinship map
+        self._insert_arity_2_stream(strip_and_codec(self.cfile),
+                'insert into category_kinship(c_from, c_to) values (?, ?)')
+        logging.info("inserted records amount: %d" % self._count_table('category_kinship'))
+
+    def _count_table(self, table):
+        self.cur.execute('select count(1) from ' + table)
+        return self.cur.fetchone()[0]
+
+    def _insert_arity_2_stream(self, stream, ins_template, sep='\t'):
+        for line in stream:
+            parts = line.split(sep)
+            assert (len(parts) == 2)
+
+            try:
+                self.cur.execute(ins_template, (parts[0], parts[1]))
+            except:
+                continue
+
+    def query_category_redirect(self, category):
+        self.cur.execute('select c_to from category_redirect where c_from=?', (category, ))
+        res = self.cur.fetchone()
+        if res is not None:
+            res = res[0]
+        return res
+
+    def query_category_uplevel(self, category):
+        self.cur.execute('select c_from, c_to from category_kinship where c_from=?', (category, ))
+        res = [x[1] for x in self.cur.fetchall()]
+        return res
+
 def main():
     parser = argparse.ArgumentParser(description="read json and filter items by domain")
 
@@ -13,73 +78,16 @@ def main():
     parser.add_argument("-r", "--category_redir_map", type=file, help="redirect map for category, a tsv file")
     parser.add_argument("-c", "--category_parent_map", type=file, help="category parent lookup, a tsv file")
     parser.add_argument("-d", "--domain", help="the required domain to filter docs, e.g. Botany")
+    parser.add_argument("-q", "--quiet", help="mute the log")
     args = parser.parse_args()
 
-    conn = sqlite3.connect(':memory:')
-    logging.getLogger().setLevel(logging.INFO)
-    build_redir_map(args.category_redir_map, conn)
-    build_category_kinship_map(args.category_parent_map, conn)
+    if not args.quiet:
+        logging.getLogger().setLevel(logging.INFO)
 
-    cur = conn.cursor()
-    print query_category_redirect(cur, u'Fatty acids')
-    print repr(query_category_uplevel(cur, u'海岸山脉')).decode('unicode_escape')
+    cr = CategoryResources(rfile=args.category_redir_map, cfile=args.category_parent_map)
 
-    pass
-
-def query_category_redirect(cur, category):
-    cur.execute('select c_to from category_redirect where c_from=?', (category, ))
-    res = cur.fetchone()
-    if res is not None:
-        res = res[0]
-    return res
-
-def query_category_uplevel(cur, category):
-    cur.execute('select c_from, c_to from category_kinship where c_from=?', (category, ))
-    res = [x[1] for x in cur.fetchall()]
-    return res
-
-def build_redir_map(mapfile, conn):
-    c = conn.cursor()
-    c.execute('create table category_redirect ('
-            'c_from text,'
-            'c_to text,'
-            'primary key (c_from, c_to)'
-            ')')
-
-    for line in mapfile:
-        parts = line.rstrip().decode('utf-8').split('\t')
-        try:
-            c.execute('insert into category_redirect values (?, ?)', (parts[0], parts[1]))
-        except:
-            continue
-
-    c.execute('select count(1) from category_redirect')
-    logging.info("inserted records amount: %d" % c.fetchone())
-
-    conn.commit()
-
-def build_category_kinship_map(mapfile, conn):
-    c = conn.cursor()
-    c.execute("""
-        create table category_kinship (
-            id integer primary key autoincrement,
-            c_from text,
-            c_to text
-        )
-        """)
-    c.execute('create index c_from_index on category_kinship(c_from)')
-
-    for line in mapfile:
-        parts = line.rstrip().decode('utf-8').split('\t')
-        try:
-            c.execute('insert into category_kinship(c_from, c_to) values (?, ?)', (parts[0], parts[1]))
-        except:
-            continue
-
-    c.execute('select count(1) from category_kinship')
-    logging.info("inserted records amount: %d" % c.fetchone())
-
-    conn.commit()
+    print cr.query_category_redirect(u'Fatty acids')
+    print repr(cr.query_category_uplevel(u'海岸山脉')).decode('unicode_escape')
 
 if __name__ == "__main__":
     main()
