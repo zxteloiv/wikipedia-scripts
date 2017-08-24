63c1505dcca72a1b5f14342db8da3fa13d5ab485
zxteloiv <zxteloiv@gmail.com>
2017-08-25 02:38:40 +0800

    modify the module name in shell script to match the moved redis importer

--- shell/run_wikidata_idx_to_redis.sh
+++ shell/run_wikidata_idx_to_redis.sh
@@ -2,6 +2,6 @@
 for (( i=0; i<=8; i++ ));
 do
     
-    echo "python -m redis_utils.load_idx --db 2 -i /data/home/zxteloiv/data/wikidata/entity_idx/wikidata.idx.0${i} --prefix WIKIDATA_QID_ &> out/log.wikidata2redis.${i}"
-    python -m redis_utils.load_idx --db 2 -i /data/home/zxteloiv/data/wikidata/entity_idx/wikidata.idx.0${i} --prefix WIKIDATA_QID_ &> out/log.wikidata2redis.${i}
+    echo "python -m utils.redis_importer --db 2 -i /data/home/zxteloiv/data/wikidata/entity_idx/wikidata.idx.0${i} --prefix WIKIDATA_QID_ &> out/log.wikidata2redis.${i}"
+    python -m utils.redis_importer --db 2 -i /data/home/zxteloiv/data/wikidata/entity_idx/wikidata.idx.0${i} --prefix WIKIDATA_QID_ &> out/log.wikidata2redis.${i}
 done;
