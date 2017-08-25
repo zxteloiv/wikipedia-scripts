9024b02b9a068c2d70e7a7f4eda9aa9e9660fd0f
zxteloiv <zxteloiv@gmail.com>
2017-08-25 22:05:47 +0800

    add wiki claim value extractor in reader

--- wikidata/reader.py
+++ wikidata/reader.py
@@ -34,11 +34,27 @@ def reader(filename):
     for l in f:
         try:
             yield json.loads(l.rstrip().rstrip(','))
-        except:
+        except ValueError:
             # error occured when invalid data and json
             # first and last line in the original full file are [ and ] respectively
             continue
 
     f.close()
             
+def claim_value(claim):
+    if 'mainsnak' not in claim: return None
+    mainsnak = claim['mainsnak']
+    if 'datavalue' not in mainsnak: return None
+    datavalue = mainsnak['datavalue']
+    if 'type' not in datavalue: return None
+    datavaluetype = datavalue['type']
+    if datavaluetype == 'wikibase-entityid':
+        value = datavalue['value']['numeric-id']
+        return u'Q' + unicode(value)
+    elif datavaluetype == "string":
+        value = datavalue['value']
+        return u'Q' + value if value[0] != u'Q' else unicode(value)
+
+    return None
+
 
